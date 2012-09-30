477276c5a4112e31ecba821ba997f596f9e45c94
Archeys <archeys123@gmail.com>
2012-09-29 21:03:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KinoRails::Application.config.secret_token = '8f916a2877a63b30fc5bf08f5a1b30478e4f7aae2a52d4ba1b841ab251e0881e3862f3a8397ec30ca07a0ee352263696b4309d8f3579a309c15c2626e7717a14'
