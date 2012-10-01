0d2a18ca70d2f4729805402988f141780641cfc6
Archeys <archeys123@gmail.com>
2012-09-30 22:00:42 -0700

    add users controller and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,22 @@
 class StaticPagesController < ApplicationController
   def home
-    render :json =>  { :test => "Data" }
+    #render :json =>  { :test => "Data" }
   end
 
   def help
+    #render :json => render_to_string(:location=>"views/help")
   end
 
   def contact
+   # @longurl = "http://www.google.com"
+   # respond_to do |format|
+   # format.html # index.html.erb
+   # format.json  { render :json => @longurl.to_json }
+   # end
+   #render :json => @longurl
   end
 
   def about
-    render :json => { :about => "I can type in whatever i want in here and it will be rendered in json format"}
+    #render :json => { :about => "I can type in whatever i want in here and it will be rendered in json format" , :url => "http://rubyonrails.org"}
   end
 end
